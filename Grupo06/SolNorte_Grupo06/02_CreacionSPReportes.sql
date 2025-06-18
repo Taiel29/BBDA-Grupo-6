28f8bf0d6d50e5eb77a4c8663f69bde979ada008
Ramiro Fariello <ramiro_fariello@hotmail.com>
2025-06-18 11:08:20 -0300

    Reportes 3 y 4

--- Grupo06/SolNorte_Grupo06/02_CreacionSPReportes.sql
+++ Grupo06/SolNorte_Grupo06/02_CreacionSPReportes.sql
@@ -18,3 +18,29 @@ BEGIN
     GROUP BY soc.Categoria, act.Nombre
     ORDER BY Cantidad_Inasistencias DESC;
 END;
+
+--EXEC sp_ReporteInasistencias;
+
+
+/* REPORTE 4
+Reporte que contenga a los socios que no han asistido a alguna clase de la actividad que
+realizan. El reporte debe contener: Nombre, Apellido, edad, categoría y la actividad*/
+
+CREATE OR ALTER PROCEDURE sp_SociosConInasistencias
+AS
+BEGIN
+    SELECT DISTINCT
+        soc.Nombre,
+        soc.Apellido,
+        DATEDIFF(YEAR, soc.Fecha_Nacimiento, GETDATE()) AS Edad,
+        cat.Nombre AS Categoria,
+        act.Descripcion AS Actividad
+    FROM actividades.Socio_Asiste_Clase asist
+    JOIN socios.Socio soc ON asist.ID_Socio = soc.ID
+    JOIN socios.Categoria_Socio cat ON soc.ID_Categoria_Socio = cat.ID
+    JOIN actividades.Clase clase ON asist.ID_Clase = clase.ID
+    JOIN actividades.Actividad act ON clase.ID_Actividad = act.ID
+    WHERE asist.Asiste IN ('A', 'J');  -- Inasistencias: Ausente o Justificado
+END;
+
+--EXEC sp_SociosConInasistencias;
